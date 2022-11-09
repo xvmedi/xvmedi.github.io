082069116a1cab5ebda13ca58b00ed61e3b7fb02
Χρίστος <116453878+xvmedi@users.noreply.github.com>
2022-11-08 22:19:28 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,14 @@
 
 (function($) {
 
+	const tracker = 
+	document.querySelector(".tracker");
+	document.body
+	.addEventListener("mousemove", e=>{
+		tracker.style.left =`${e.clientX}px`;
+		tracker.style.top = `${e.clientY}px`;
+	});
+
 	var	$window = $(window),
 		$body = $('body'),
 		$wrapper = $('#wrapper'),
